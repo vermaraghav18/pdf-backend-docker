3522d3e3253b9adf629318b3d0890493943f64f6
raghaverma18 <raghav18verma@gmail.com>
2025-06-04 13:34:47 +0530

    ✅ Add PDF to Excel feature

--- server.js
+++ server.js
@@ -2,13 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Allow local dev frontend
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
-
+app.use(cors());
 
 const mergePdfRoute = require('./routes/mergePdf');
 const splitPdfRoute = require('./routes/splitPdf');
