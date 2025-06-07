719bfbe38e9e21e75db1b34ce3c2bcac1cfb630d
raghaverma18 <raghav18verma@gmail.com>
2025-06-07 11:13:51 +0530

    ✅ Added Word, Excel, PPT to PDF features with Python microservices and final frontend JSX

--- routes/uploadMiddleware.js
+++ routes/uploadMiddleware.js
@@ -1,23 +1,23 @@
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+  const multer = require('multer');
+  const path = require('path');
+  const fs = require('fs');
 
-// Make sure uploads folder exists
-const uploadDir = path.join(__dirname, '..', 'uploads');
-if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+  // Make sure uploads folder exists
+  const uploadDir = path.join(__dirname, '..', 'uploads');
+  if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, uploadDir),
-  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
-});
-
-
-const pdfFileFilter = (req, file, cb) => {
-  if (file.mimetype === 'application/pdf') cb(null, true);
-  else cb(new Error('Only PDF files are allowed'), false);
-};
-
-const upload = multer({ storage , fileFilter: pdfFileFilter });
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, uploadDir),
+    filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+  });
 
 
-module.exports = { upload };
+  const pdfFileFilter = (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') cb(null, true);
+    else cb(new Error('Only PDF files are allowed'), false);
+  };
+
+  const upload = multer({ storage , fileFilter: pdfFileFilter });
+
+
+  module.exports = { upload };
